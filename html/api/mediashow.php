4caf28118d8637c62bd9664ff0365852db6dd3ee
unknown <>
2016-11-25 23:54:36 +0900

    update.

--- html/api/mediashow.php
+++ html/api/mediashow.php
@@ -1,11 +1,15 @@
 <?php
 
+function toFileName($n) {
+	return basename($n);
+}
+
 $videodir = "/media/hdd1/videos/*.mp4";
 $audiodir = "/media/hdd1/musics/*.mp3";
 
 http_response_code(200);
 header('content-type: application/json; charset=utf-8');
 echo json_encode(array(
-		"audios"  => glob($audiodir),
-		"videos" => glob($videodir)
+		"audios"  => array_map("toFileName", glob($audiodir)),
+		"videos" => array_map("toFileName", glob($videodir))
 ));
