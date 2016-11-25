10cb14d958bcc0ceaaff41ae71ca43a0d58f55dd
unknown <>
2016-11-25 23:52:15 +0900

    update.

--- html/api/mediashow.php
+++ html/api/mediashow.php
@@ -1,7 +1,7 @@
 <?php
 
-$videodir = "/media/hdd1/videos";
-$audiodir = "/media/hdd1/musics";
+$videodir = "/media/hdd1/videos/*.mp4";
+$audiodir = "/media/hdd1/musics/*.mp3";
 
 http_response_code(200);
 header('content-type: application/json; charset=utf-8');
